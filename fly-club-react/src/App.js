b96b490b93daad53d58448f23cd56d912da62bcd
Drew85 <foxracing125ccm@gmail.com>
2023-03-23 10:29:56 +0100

    Add React fetch

--- fly-club-react/src/App.js
+++ fly-club-react/src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState(undefined);
+
+    useEffect(() => {
+      const fetchData = async () => {
+          const result  = await axios(
+              `/user`);
+          setUsers(result.data);
+      };
+      fetchData();
+    }, []);
+
+    return (
+        <div className="App">
+            <ul>
+                {!users ? "Loading..." : users.map((user) =>
+                    <li key={user.id}>
+                        {user.name}
+                        {user.id}
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
+}
+export default App;
+
+
+{/*<header className="App-header">*/
+}
+{/*  <img src={logo} className="App-logo" alt="logo" />*/
+}
+{/*  <p>*/
+}
+{/*    Edit <code>src/App.js</code> and save to reload.*/
+}
+{/*  </p>*/
+}
+{/*  <a*/
+}
+{/*    className="App-link"*/
+}
+{/*    href="https://reactjs.org"*/
+}
+{/*    target="_blank"*/
+}
+{/*    rel="noopener noreferrer"*/
+}
+{/*  >*/
+}
+{/*    Learn React*/
+}
+{/*  </a>*/
+}
+{/*</header>*/
 }
 
-export default App;
+// useEffect(() => {
+//     fetch(`http://localhost:8080/user`)
+//         .then(res => (res.json()))
+//         .then(({users}) => setUsers((users)))
+// });
